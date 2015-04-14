68b397c831205591b678444eca861df6d626dd66
Ben Briggs <beneb.info@gmail.com>
2015-04-14 18:31:23 +0100

    This is already documented.

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 'use strict';
 
-/**
- * Expose an object containing a normalised API for each of the minifier
- * engines. Each engine has a main exported function and a version key.
- */
-
 module.exports = require('bulk-require')(__dirname + '/engines/', '*.js');
